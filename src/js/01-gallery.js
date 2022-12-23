0877ba9ed4df5e5692ee71d519a6e4277da734e0
maxk3y <109315215+maxk3y@users.noreply.github.com>
2022-12-23 18:29:06 +0200

    dz 8 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryItemMarkup = galleryMarkup(galleryItems);
+gallery.innerHTML = galleryItemMarkup;
+
+function galleryMarkup(galleryItems) {
+  return galleryItems
+
+    .map(
+      item => `
+      <li>
+      <a class = "gallery__item" href = "${item.original}">
+      <img
+      class = "gallery__image"
+      src="${item.preview}"
+      alt = "${item.description}"
+      />
+      </a>
+      </li>`
+    )
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const fbEmail = document.querySelector('[name="email"]');
+const fbMessage = document.querySelector('[name="message"]');
+
+const storageKey = 'feedback-form-state';
+
+let formData = {};
+
+feedbackForm.addEventListener('input', throttle(onInput, 500));
+feedbackForm.addEventListener('submit', onSubmitForm);
+
+function onInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(storageKey, JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  feedbackForm.reset();
+  console.log(JSON.parse(localStorage.getItem(storageKey)));
+  localStorage.removeItem(storageKey);
+}
+
+preload();
+
+function preload() {
+  const preData = JSON.parse(localStorage.getItem(storageKey));
+  if (preData) {
+    fbEmail.value = preData.email;
+    fbMessage.value = preData.message;
+  }
+}
